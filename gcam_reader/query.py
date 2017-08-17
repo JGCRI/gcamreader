f9b83917c586a7ba34c0509c36a28897ae11cb33
Robert Link <robert.link@pnnl.gov>
2017-08-17 17:35:42 -0400

    Add importdata function.

--- gcam_reader/query.py
+++ gcam_reader/query.py
@@ -1,7 +1,7 @@
 """Functions and classes for handling GCAM output DB queries."""
 
 import xml.etree.ElementTree as ET
-
+import os.path
 
 class Query:
     def __init__(self, xmlin):
@@ -9,6 +9,11 @@ class Query:
         Arguments:
           * xmlq: XML query definition. Can be either a string or a parsed XML
             element.
+
+        Attributes:
+          * querystr: string representation of the query
+          * regions: region list parsed from the query XML
+          * title: query title
         """
 
         if xmlin.__class__ == str:
@@ -38,3 +43,41 @@ def parse_batch_query(filename):
     queries = root.findall('aQuery')
 
     return [Query(q) for q in queries]
+
+def importdata(dbspec, queries, scenarios=None, regions=None, warn_empty=False,
+           suppress_gabble=True, miclasspath=None):
+    """Run a selection of queries against a database connection
+
+    Run all of the queries in a GCAM queries file against a database
+    connection and return a dictionary of query structures indeed by
+    query title.
+
+    Arguments:
+      * dbspec: a database connection, or a string with the filename 
+          for a GCAM database
+      * queries: filename of a GCAM queries XML file, or the output 
+          of parse_batch_query run on such a file.
+      * scenarios: list of scenario names to include in the queries
+      * regions: list of regions to include in the queries
+      * warn_empty: Flag: print a warning if a query returns empty. 
+          Default is false.
+    """
+
+    if dbspec.__class__ == str:
+        dbdir = os.path.dirname(dbspec)
+        dbname = os.path.dirname(dbspec)
+        dbcon = LocalDBConn(dbdir, dbname, suppress_gabble, miclasspath)
+    else:
+        dbcon = dbspec
+
+    if queries.__class__ == str:
+        queries = parse_batch_query(queries)
+
+    queryrslts = {} 
+    for query in queries:
+        qr = dbcon.runQuery(query, scenarios, regions, warn_empty)
+        queryrslts[query.title] = qr
+
+    return queryrslts
+
+        
