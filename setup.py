1387a5822ba07d414ad58c80be4ce4eb7d9cef0a
crvernon <chrisrvernon@gmail.com>
2019-02-04 07:51:34 -0500

    raise depreciation exception for < Python 3.5

--- setup.py
+++ setup.py
@@ -7,34 +7,21 @@ with open('requirements.txt') as f:
 
 # [0] major version, [1] minor, [2] mirco, [3] releaselevel, [4] serial
 reader_info = sys.version_info
+print(reader_info)
 
 # account for version dependency for subprocess
 if (reader_info[0] == 3) and (reader_info[1] >= 5):
     pass
 
 elif (reader_info[0] == 3) and (reader_info[1] < 5):
-    msg = """DEPRECIATION:  gcam_reader will soon only support >= Python 3.5. Please upgrade
-             your Python version."""
-
-    requirements.append("subprocess.run>=0.0.8")
-
-    raise PendingDeprecationWarning(msg)
-
-elif (reader_info[0] == 2) and (reader_info[1] in (6, 7)):
-    msg = """DEPRECATION: Python 2.7 will reach the end of its life on January 1st, 2020.
-             Please upgrade your Python as Python 2.7 won't be maintained after that date.
-             A future version of pip will drop support for Python 2.7.
-             gcam_reader will soon only support >= Python 3.5. Please upgrade
-             your Python version."""
-
-    raise PendingDeprecationWarning(msg)
+    msg = """\nDEPRECIATION:  gcam_reader will soon only support >= Python 3.5. Please upgrade
+             your Python version.\n"""
 
 else:
-    msg = """DEPRECATION: Your Python version {}.{} is depreciated Python 2.7 will reach the end of its life on January 1st, 2020.
-             Please upgrade your Python as Python 2.7 won't be maintained after that date.
-             A future version of pip will drop support for Python 2.7.
-             gcam_reader will soon only support >= Python 3.5. Please upgrade
-             your Python version.""".format(reader_info[0], reader_info[1])
+    msg = """\nDEPRECATION: Your Python version {}.{} is depreciated due to Python 2.7 reaching the end of its life on 
+                January 1st, 2020. Please upgrade your Python as Python 2.7 won't be maintained after that date.
+                A future version of pip will drop support for Python 2.7.
+                \ngcam_reader will now only support >= Python 3.5. Please upgrade your Python version.\n""".format(reader_info[0], reader_info[1])
 
     raise DeprecationWarning(msg)
 
