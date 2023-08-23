2150568247c759a02e4f8f2c600c14a700a1ecde
crvernon <chrisrvernon@gmail.com>
2023-08-23 13:19:19 -0400

    advance install click

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
         "requests~=2.20.0",
         "pandas>=1.2.4",
         "lxml>=4.6.3"
-        "click>=8.1.6",
-        "click-default-group>=1.2.3",
+        "click",
+        "click-default-group-wheel",
     ],
     entry_points="""
         [console_scripts]
